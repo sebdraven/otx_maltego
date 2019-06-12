ab2462b3031d3e3e683b5cdb92fd49f8d41bad00
Sebdraven <slarinier@gmail.com>
2019-06-12 14:38:09 +0200

    prepare env

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='VT_Transform',
+    author='Sebastien Larinier',
+    version='1.0',
+    author_email='sebdraven@protonmail.com',
+    maintainer='Sebastien Larinier',
+    url='',
+    description='Maltego transform for interacting with a Virustotal.',
+    license='GPLv3',
+    packages=find_packages('VT_Transform'),
+    package_dir={'': 'VT_Transform'},
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3',
+        'Development Status :: 4 - Beta',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Internet',
+    ],
+    package_data={
+        '': ['*.gif', '*.png', '*.conf', '*.mtz', '*.machine']  # list of resources
+    },
+    python_requires='>=3.5',
+    install_requires=[
+        'canari>=3.3.9,<4',
+        'request'
+    ],
+    dependency_links=[
+        # custom links for the install_requires
+    ]
+)
